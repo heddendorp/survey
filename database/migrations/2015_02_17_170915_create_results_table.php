bd4e185eb6b5e1f82ad0589a5525c2f0f8d2ab9c
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-02-17 18:15:22 +0100

    Results table migration

---
+++ database/migrations/2015_02_17_170915_create_results_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateResultsTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('results', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('questionnaire_id');
+            $table->integer('group_id');
+            $table->text('data_json');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('results');
+	}
+
+}
