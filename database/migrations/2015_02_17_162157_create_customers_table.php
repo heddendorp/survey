e4387d9d2cdac3c1575087bd7ff7b7f3504b9844
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-02-17 17:25:25 +0100

    Customers table migration

---
+++ database/migrations/2015_02_17_162157_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCustomersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('customers', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->text('name');
+            $table->text('logo');
+            $table->text('email');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('customers');
+	}
+
+}
