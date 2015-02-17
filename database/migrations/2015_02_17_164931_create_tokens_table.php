895bfbeaebce1db8bab80f594607111229dbf501
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-02-17 18:02:27 +0100

    Tokens table migration

---
+++ database/migrations/2015_02_17_164931_create_tokens_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTokensTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tokens', function(Blueprint $table)
+		{
+			$table->increments('id');
+            $table->integer('child_id');
+            $table->integer('mail_id');
+            $table->integer('questionnaire_id');
+            $table->string('token');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tokens');
+	}
+
+}
