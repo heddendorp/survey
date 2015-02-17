d3b6f58449f0c173744cf7c85cbdba19833f1b20
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-02-17 17:21:52 +0100

    User table migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,6 +18,7 @@ class CreateUsersTable extends Migration {
 			$table->string('name');
 			$table->string('email')->unique();
 			$table->string('password', 60);
+            $table->integer('customer_id');
 			$table->rememberToken();
 			$table->timestamps();
 		});
