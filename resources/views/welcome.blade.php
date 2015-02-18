3a0e5cfc8e12619a63cbbd79af2c0e837723eaa1
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-02-18 13:15:38 +0100

    more composer magic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,5 +6,6 @@
     <script src="{{ elixir("js/all.js") }}"></script>
 </head>
 <body>
+<div class="uk-cover-background" style="background-image: url({{asset('img/landing-mockup.jpg')}}); height: 370px;"></div>
 </body>
 </html>
