9e5197e38601cbada94652662bf63e3ec7347400
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-02-21 08:29:02 +0100

    Iteration Validation

---
+++ app/Http/Requests/IterationRequest.php
@@ -0,0 +1,29 @@
+<?php namespace Survey\Http\Requests;
+
+use Survey\Http\Requests\Request;
+
+class IterationRequest extends Request {
+
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize()
+	{
+		return true;
+	}
+
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules()
+	{
+		return [
+			'description'=>'required'
+		];
+	}
+
+}
