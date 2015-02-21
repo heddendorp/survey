982d2ec0aadedd9484647dd26c7991134a309df3
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-02-21 09:09:49 +0100

    Group Creation now possible

--- app/Http/Controllers/CustomerIterationFacilityGroupController.php
+++ app/Http/Controllers/CustomerIterationFacilityGroupController.php
@@ -2,6 +2,7 @@
 
 use Survey\Customer;
 use Survey\Facility;
+use Survey\Group;
 use Survey\Http\Requests;
 use Survey\Http\Controllers\Controller;
 
@@ -30,24 +31,36 @@ class CustomerIterationFacilityGroupController extends Controller {
         return view('group.index')->withCustomer($customer)->withIteration($iteration)->withFacility($facility)->withGroups($groups);
 	}
 
-	/**
-	 * Show the form for creating a new resource.
-	 *
-	 * @return Response
-	 */
-	public function create()
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @param Customer $customer
+     * @param Iteration $iteration
+     * @param Facility $facility
+     * @return Response
+     */
+	public function create(Customer $customer, Iteration $iteration, Facility $facility)
 	{
-		//
+        return view('group.create')->withCustomer($customer)->withIteration($iteration)->withFacility($facility);
 	}
 
-	/**
-	 * Store a newly created resource in storage.
-	 *
-	 * @return Response
-	 */
-	public function store()
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param Customer $customer
+     * @param Iteration $iteration
+     * @param Facility $facility
+     * @param Requests\FacilityRequest $request
+     * @return Response
+     */
+	public function store(Customer $customer, Iteration $iteration, Facility $facility, Requests\FacilityRequest $request)
 	{
-		//
+        $group = new Group;
+        $group->name = $request->get('name');
+        $group->type = $request->get('type');
+        $group->facility_id = $facility->id;
+        $group->save();
+        return redirect()->route('customer.iteration.facility.group.index', [$customer, $iteration, $facility]);
 	}
 
 	/**
