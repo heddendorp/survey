c14336b5f5ecd60458cf3e64db219902110cd7b9
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-02-22 15:47:35 +0100

    Papertrail Logging function

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,16 @@
 |
 */
 
+//Papertrail
+$monolog = Log::getMonolog();
+$syslog = new \Monolog\Handler\SyslogHandler('papertrail');
+$formatter = new \Monolog\Formatter\LineFormatter('%channel%.%level_name%: %message% %extra%');
+$syslog->setFormatter($formatter);
+
+$monolog->pushHandler($syslog);
+
+//routes
+
 Route::get('/', 'WelcomeController@index');
 
 Route::get('data', function(){
