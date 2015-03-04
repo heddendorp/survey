bd5717c0d37e78f7ac702c2574ad8c0a10eb2bbb
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-03-04 20:49:04 +0100

    Update Questionnaire Controllers to use the permission middleware

--- app/Http/Controllers/CustomerQuestionnaireSectionQuestiongroupController.php
+++ app/Http/Controllers/CustomerQuestionnaireSectionQuestiongroupController.php
@@ -19,6 +19,7 @@ class CustomerQuestionnaireSectionQuestiongroupController extends Controller {
     {
         $this->middleware('auth');
         $this->middleware('customerplus');
+        $this->middleware('questionnairePerms');
     }
 
     /**
