fce9a7952f728a0f816ab0cfdcf2932e5b321988
Lukas Heddendorp <lu.heddendorp@gmail.com>
2015-03-06 20:00:42 +0100

    Another merging fail is fixed by this

---
+++ database/migrations/2015_03_01_170939_modify_users_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ModifyUsersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->json('role')->default('{"admin":true}');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::table('users', function(Blueprint $table)
+		{
+			$table->dropColumn('role');
+		});
+	}
+
+}
