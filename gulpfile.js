eb9232dbb82d5ae14b0c0e817b0b8aabc68c5ef7
Isigiel <lu.heddendorp@gmail.com>
2015-04-02 14:44:51 +0200

    Remade the bower gulp and node packages and added sem-ui for further development

--- gulpfile.js
+++ gulpfile.js
@@ -12,25 +12,22 @@ var elixir = require('laravel-elixir');
  */
 
 elixir(function(mix) {
-    mix.copy("bower_components/uikit/scss","resources/assets/sass/uikit");
-    mix.copy("bower_components/semantic-ui-card/card.css","public/css/card.css");
-    mix.copy("bower_components/chartist/dist/chartist.min.css","public/css/chartist.min.css");
+
+    mix.copy("bower_components/chartist/dist/chartist.min.css","resources/css/chartist.min.css");
     mix.copy("bower_components/chartist/dist/chartist.js","resources/js/chartist.js");
-    mix.copy("bower_components/uikit/js","resources/js");
     mix.copy("bower_components/jquery/dist/jquery.js","resources/js/jquery.js");
-    mix.copy("bower_components/uikit/fonts","public/build/fonts");
+    mix.copy("semantic/dist/semantic.js","resources/js/semantic.js");
+    mix.copy("semantic/dist/semantic.css","resources/css/semantic.css");
+
     mix.scripts([
         "jquery.js",
-        "uikit.js",
-        "components/sortable.js",
-        "components/upload.js",
-        "components/form-select.js",
-        "components/datepicker.js",
         "jquery.restfulizer.js",
-        "chartist.js",
-        "app.js"
+        "semantic.js",
+        "chartist.js"
     ]);
-    mix.sass('app.sass');
-    mix.stylesIn("public/css");
+    mix.styles([
+        "chartist.min.css",
+        "semantic.css"
+    ]);
     mix.version(["css/all.css","js/all.js"]);
 });
