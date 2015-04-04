5a140529f9632285bf4e5a8c4c1b5af616dd8277
Isigiel <lu.heddendorp@gmail.com>
2015-04-04 14:53:07 +0200

    Finished the user views

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -2,39 +2,37 @@
 @section('outlet')
     <div class="ui one column grid">
         <div class="column">
-            <h5 class="ui top attached header">
-                Benutzer
-            </h5>
-            <div class="ui attached segment">
-                <table class="ui stripped table">
-                    <thead>
+            <table class="ui blue striped table">
+                <thead>
+                <tr>
+                    <th colspan="3"><h3>Benutzer</h3></th>
+                </tr>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                    <th></th>
+                </tr>
+                </thead>
+                <tbody>
+                @foreach($users as $user)
                     <tr>
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th></th>
+                        <td>{{$user->name}}</td>
+                        <td>{{$user->email}}</td>
+                        <td>
+                            <button id="launch-{{$user->id}}" class="ui labeled icon button"><i class="configure icon"></i> Bearbeiten</button>
+                            @if($user->admin == true)
+                                <button class="ui disabled labeled icon button"><i class="trash icon"></i> Löschen</button>
+                            @else
+                                <a class="rest ui red labeled icon button" data-method="DELETE" href="{{route('customer.user.destroy', [$customer,$user]).'?_token='.csrf_token()}}"><i class="trash icon"></i> Löschen</a>
+                            @endif
+                        </td>
                     </tr>
-                    </thead>
-                    <tbody>
-                    @foreach($users as $user)
-                        <tr>
-                            <td>{{$user->name}}</td>
-                            <td>{{$user->email}}</td>
-                            <td>
-                                <button id="launch-{{$user->id}}" class="ui labeled icon button"><i class="configure icon"></i> Bearbeiten</button>
-                                @if($user->admin == true)
-                                    <button class="ui disabled labeled icon button"><i class="trash icon"></i> Löschen</button>
-                                @else
-                                    <a class="rest ui red labeled icon button" data-method="DELETE" href="{{route('customer.user.destroy', [$customer,$user]).'?_token='.csrf_token()}}"><i class="trash icon"></i> Löschen</a>
-                                @endif
-                            </td>
-                        </tr>
-                    @endforeach
-                    </tbody>
-                </table>
+                @endforeach
+                </tbody>
+            </table>
                 <div id="launcher" class="ui right floated primary labeled icon button">
                     <i class="user icon"></i> Neuer Benutzer
                 </div>
-            </div>
         </div>
     </div>
 
